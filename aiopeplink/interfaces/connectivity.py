330c3c08e63a8fd77020b906192a6fe28eb1a809
Mitchell Carlson <mitchell5433@gmail.com>
2023-12-31 12:44:34 -0600

    Add /api/info.location

--- aiopeplink/interfaces/connectivity.py
+++ aiopeplink/interfaces/connectivity.py
@@ -3,10 +3,12 @@ from ssl import SSLContext
 from typing import Literal
 
 
-class Auth:
+class Connectivity:
     """Class to make authenticated requests."""
 
-    def __init__(self, websession: ClientSession, host: str, username: str, password: str):
+    def __init__(
+        self, websession: ClientSession, host: str, username: str, password: str
+    ):
         """Initialize the auth."""
         self.websession = websession
         self.host = host
@@ -19,18 +21,22 @@ class Auth:
         """Login to the router with username and password. Receive a bauth cookie back to use as authentication for the session."""
         login_url = f"{self.host}/api/login"
         async with self.websession.post(
-            login_url, data={"username": self.username, "password": self.password}, ssl=self.ssl_context,
+            login_url,
+            data={"username": self.username, "password": self.password},
+            ssl=self.ssl_context,
         ) as response:
-                if response.status == 200:
-                    self.cookie = response.cookies.get("bauth")
-                else:
-                    raise Exception(f"Failed to get authentication cookie. Status code: {response.status}")
+            if response.status == 200:
+                self.cookie = response.cookies.get("bauth")
+            else:
+                raise Exception(
+                    f"Failed to get authentication cookie. Status code: {response.status}"
+                )
 
     async def request(self, method: str, path: str, **kwargs) -> ClientResponse:
         """Make a request."""
         if not self.cookie:
             await self.login()
-        
+
         headers = kwargs.get("headers")
 
         if headers is None:
@@ -41,5 +47,10 @@ class Auth:
         cookies = {"bauth": self.cookie}
 
         return await self.websession.request(
-            method, f"{self.host}/api/{path}", **kwargs, headers=headers, cookies=cookies, ssl=self.ssl_context
+            method,
+            f"{self.host}/api/{path}",
+            **kwargs,
+            headers=headers,
+            cookies=cookies,
+            ssl=self.ssl_context,
         )
